8fe7dde5faa7223159ebb3d50752200d34f8a8ca
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-08-31 13:19:46 +0200

    Adding and displaying question and count answer

--- app.js
+++ app.js
@@ -7,8 +7,41 @@ function populate(){
         //show question
         var element = document.getElementById("question");
         element.innerHTML = quiz.getQuestionIndex().text;
+        
+        //show choices
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i =0; i<choices.length; i++){
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+       showProgress();
+    }
+};
+
+function guess(id, guess){
+    var button = document.getElementById(id);
+    button.onclick = function(){
+        quiz.guess(guess);
+        populate();
     }
 }
+
+function showProgress(){
+    
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + "of " + quiz.questions.length;
+}
+
+
+function showScore(){
+    var gameOverHtml = "<h1>Result</h1>";
+    gameOverHtml += "<h2 id ='score'>Your scores: " + quiz.score+ "</h2>";
+     var element = document.getElementById("quiz");
+        element.innerHTML = gameOverHtml;
+}
+
 
 
 var questions = [
--- quiz_controller.js
+++ quiz_controller.js
@@ -8,15 +8,22 @@ Quiz.prototype.getQuestionIndex = function(){
     return this.questions[this.questionIndex];
 }
 
+
+
 Quiz.prototype.isEnded = function(){
     
     return this.questions.length === this.questionIndex;
 }
 
+
+
 Quiz.prototype.guess = function(answer){
-    this.questionIndex++;
     
-    if(this.getQuestionIndex().correctAnswer(answer)){
+    
+    if(this.getQuestionIndex().corr.correctAnswer(answer)){
         this.score++;
     }
+    
+    this.questionIndex++;
 }
+
